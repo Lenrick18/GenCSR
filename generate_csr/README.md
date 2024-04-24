b88370b849e208562e2491e8164a6f105955ddff
Lenrick18 <107453533+Lenrick18@users.noreply.github.com>
2024-04-24 10:23:37 +0800

    Add files via upload

---
+++ generate_csr/README.md
@@ -0,0 +1,9 @@
+Run using Mobaxterm/Linux
+Run this command first
+----------------------------------
+    chmod +x ./generate_csr.sh
+----------------------------------
+Generate CSR using this commmand
+----------------------------------
+	./generate_csr <CSR name>
+----------------------------------
--- generate_csr/generate_csr.sh
+++ generate_csr/generate_csr.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-echo "Name of this CSR Request: "
-read CSRName
-echo "Have you modified DNS name in add_san.cnf file?"
+echo "Have you modified DNS name in add_san.cnf file for $1 ?"
 echo "1. YES"
 echo "2. NO"
 read Ans
 if [ $Ans -eq "1" ]
 then
-        openssl req -out $CSRName.csr -newkey rsa:2048 -nodes -keyout $CSRName.key -config /home/mobaxterm/certificate_request/add_san.cnf
-        mkdir $CSRName
-        mv $CSRName.key $CSRName/$CSRName.key
-        mv $CSRName.csr $CSRName/$CSRName.csr
-        echo "Done creating CSR and key file for $CSRName Thank you!!!"
+        openssl req -out $1.csr -newkey rsa:2048 -nodes -keyout $1.key -config add_san.cnf
+        mkdir $1
+        mv $1.key $1/$1.key
+        mv $1.csr $1/$1.csr
+        echo "Done creating CSR and key file for $1 Thank you!!!"
 else
         echo "Please modify DNS name in add_san.cnf file first, Thank you"
         sleep 3
